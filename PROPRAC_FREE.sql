654f4d1227ff0a643c951e325e3700d3cd1b9de0
SimSimITWorld <dkfnfn4834@gmail.com>
2023-03-31 18:11:54 +0900

    20230331

--- PROPRAC_FREE.sql
+++ PROPRAC_FREE.sql
@@ -23,23 +23,8 @@ NOMAXVALUE
 NOCACHE
 NOCYCLE;
 
+DROP TABLE PROPRAC_FREE;
+DROP SEQUENCE PROPRAC_FREE_SEQ;
+
 INSERT INTO PROPRAC_FREE(NO, TITLE, WRITER, PASS, CONTENT, REF, STEP, DEPTH, REGDATE) VALUES(1, '자유롭게 이용해주세요.', '관리자', 'system_master', '안녕하세요. 요기조기 입니다. 상스러운 글만 자제해주시고 자유롭게 이용하시면 됩니다. 감사합니다.', 1, 0, 0, SYSDATE);
-INSERT INTO PROPRAC_FREE(NO, TITLE, WRITER, PASS, CONTENT, REF, STEP, DEPTH, REGDATE) VALUES(2, '자유롭게 이용해주세요.', '관리자', 'system_master', '안녕하세요. 요기조기 입니다. 상스러운 글만 자제해주시고 자유롭게 이용하시면 됩니다. 감사합니다.', 1, 0, 0, SYSDATE);
-INSERT INTO PROPRAC_FREE(NO, TITLE, WRITER, PASS, CONTENT, REF, STEP, DEPTH, REGDATE) VALUES(3, '자유롭게 이용해주세요.', '관리자', 'system_master', '안녕하세요. 요기조기 입니다. 상스러운 글만 자제해주시고 자유롭게 이용하시면 됩니다. 감사합니다.', 1, 0, 0, SYSDATE);
-INSERT INTO PROPRAC_FREE(NO, TITLE, WRITER, PASS, CONTENT, REF, STEP, DEPTH, REGDATE) VALUES(4, '자유롭게 이용해주세요.', '관리자', 'system_master', '안녕하세요. 요기조기 입니다. 상스러운 글만 자제해주시고 자유롭게 이용하시면 됩니다. 감사합니다.', 1, 0, 0, SYSDATE);
-INSERT INTO PROPRAC_FREE(NO, TITLE, WRITER, PASS, CONTENT, REF, STEP, DEPTH, REGDATE) VALUES(5, '자유롭게 이용해주세요.', '관리자', 'system_master', '안녕하세요. 요기조기 입니다. 상스러운 글만 자제해주시고 자유롭게 이용하시면 됩니다. 감사합니다.', 1, 0, 0, SYSDATE);
-INSERT INTO PROPRAC_FREE(NO, TITLE, WRITER, PASS, CONTENT, REF, STEP, DEPTH, REGDATE) VALUES(6, '자유롭게 이용해주세요.', '관리자', 'system_master', '안녕하세요. 요기조기 입니다. 상스러운 글만 자제해주시고 자유롭게 이용하시면 됩니다. 감사합니다.', 1, 0, 0, SYSDATE);
-INSERT INTO PROPRAC_FREE(NO, TITLE, WRITER, PASS, CONTENT, REF, STEP, DEPTH, REGDATE) VALUES(7, '자유롭게 이용해주세요.', '관리자', 'system_master', '안녕하세요. 요기조기 입니다. 상스러운 글만 자제해주시고 자유롭게 이용하시면 됩니다. 감사합니다.', 1, 0, 0, SYSDATE);
-INSERT INTO PROPRAC_FREE(NO, TITLE, WRITER, PASS, CONTENT, REF, STEP, DEPTH, REGDATE) VALUES(8, '자유롭게 이용해주세요.', '관리자', 'system_master', '안녕하세요. 요기조기 입니다. 상스러운 글만 자제해주시고 자유롭게 이용하시면 됩니다. 감사합니다.', 1, 0, 0, SYSDATE);
-INSERT INTO PROPRAC_FREE(NO, TITLE, WRITER, PASS, CONTENT, REF, STEP, DEPTH, REGDATE) VALUES(9, '자유롭게 이용해주세요.', '관리자', 'system_master', '안녕하세요. 요기조기 입니다. 상스러운 글만 자제해주시고 자유롭게 이용하시면 됩니다. 감사합니다.', 1, 0, 0, SYSDATE);
-INSERT INTO PROPRAC_FREE(NO, TITLE, WRITER, PASS, CONTENT, REF, STEP, DEPTH, REGDATE) VALUES(10, '자유롭게 이용해주세요.', '관리자', 'system_master', '안녕하세요. 요기조기 입니다. 상스러운 글만 자제해주시고 자유롭게 이용하시면 됩니다. 감사합니다.', 1, 0, 0, SYSDATE);
-INSERT INTO PROPRAC_FREE(NO, TITLE, WRITER, PASS, CONTENT, REF, STEP, DEPTH, REGDATE) VALUES(11, '자유롭게 이용해주세요.', '관리자', 'system_master', '안녕하세요. 요기조기 입니다. 상스러운 글만 자제해주시고 자유롭게 이용하시면 됩니다. 감사합니다.', 1, 0, 0, SYSDATE);
-INSERT INTO PROPRAC_FREE(NO, TITLE, WRITER, PASS, CONTENT, REF, STEP, DEPTH, REGDATE) VALUES(12, '자유롭게 이용해주세요.', '관리자', 'system_master', '안녕하세요. 요기조기 입니다. 상스러운 글만 자제해주시고 자유롭게 이용하시면 됩니다. 감사합니다.', 1, 0, 0, SYSDATE);
-INSERT INTO PROPRAC_FREE(NO, TITLE, WRITER, PASS, CONTENT, REF, STEP, DEPTH, REGDATE) VALUES(13, '자유롭게 이용해주세요.', '관리자', 'system_master', '안녕하세요. 요기조기 입니다. 상스러운 글만 자제해주시고 자유롭게 이용하시면 됩니다. 감사합니다.', 1, 0, 0, SYSDATE);
-INSERT INTO PROPRAC_FREE(NO, TITLE, WRITER, PASS, CONTENT, REF, STEP, DEPTH, REGDATE) VALUES(14, '자유롭게 이용해주세요.', '관리자', 'system_master', '안녕하세요. 요기조기 입니다. 상스러운 글만 자제해주시고 자유롭게 이용하시면 됩니다. 감사합니다.', 1, 0, 0, SYSDATE);
-INSERT INTO PROPRAC_FREE(NO, TITLE, WRITER, PASS, CONTENT, REF, STEP, DEPTH, REGDATE) VALUES(15, '자유롭게 이용해주세요.', '관리자', 'system_master', '안녕하세요. 요기조기 입니다. 상스러운 글만 자제해주시고 자유롭게 이용하시면 됩니다. 감사합니다.', 1, 0, 0, SYSDATE);
-INSERT INTO PROPRAC_FREE(NO, TITLE, WRITER, PASS, CONTENT, REF, STEP, DEPTH, REGDATE) VALUES(16, '자유롭게 이용해주세요.', '관리자', 'system_master', '안녕하세요. 요기조기 입니다. 상스러운 글만 자제해주시고 자유롭게 이용하시면 됩니다. 감사합니다.', 1, 0, 0, SYSDATE);
-INSERT INTO PROPRAC_FREE(NO, TITLE, WRITER, PASS, CONTENT, REF, STEP, DEPTH, REGDATE) VALUES(17, '자유롭게 이용해주세요.', '관리자', 'system_master', '안녕하세요. 요기조기 입니다. 상스러운 글만 자제해주시고 자유롭게 이용하시면 됩니다. 감사합니다.', 1, 0, 0, SYSDATE);
-INSERT INTO PROPRAC_FREE(NO, TITLE, WRITER, PASS, CONTENT, REF, STEP, DEPTH, REGDATE) VALUES(18, '자유롭게 이용해주세요.', '관리자', 'system_master', '안녕하세요. 요기조기 입니다. 상스러운 글만 자제해주시고 자유롭게 이용하시면 됩니다. 감사합니다.', 1, 0, 0, SYSDATE);
-INSERT INTO PROPRAC_FREE(NO, TITLE, WRITER, PASS, CONTENT, REF, STEP, DEPTH, REGDATE) VALUES(19, '자유롭게 이용해주세요.', '관리자', 'system_master', '안녕하세요. 요기조기 입니다. 상스러운 글만 자제해주시고 자유롭게 이용하시면 됩니다. 감사합니다.', 1, 0, 0, SYSDATE);
-INSERT INTO PROPRAC_FREE(NO, TITLE, WRITER, PASS, CONTENT, REF, STEP, DEPTH, REGDATE) VALUES(20, '자유롭게 이용해주세요.', '관리자', 'system_master', '안녕하세요. 요기조기 입니다. 상스러운 글만 자제해주시고 자유롭게 이용하시면 됩니다. 감사합니다.', 1, 0, 0, SYSDATE);
+
